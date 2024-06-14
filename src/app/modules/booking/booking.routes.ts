a2f7b682716637c06c31c3f366a82095b965bfc6
nasimrifat101 <nasimrifat101@gmail.com>
2024-06-14 20:24:40 +0600

    zod validation for booking initiated

--- src/app/modules/booking/booking.routes.ts
+++ src/app/modules/booking/booking.routes.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { bookingController } from "./booking.controllers";
 import { jwtAuth } from "../../middlewears/jwtAuthentication";
 import { isAdmin } from "../../middlewears/isAdmin";
+import { validateReq } from "../../middlewears/ValidateRequest";
+import { bookingValidation } from "./booking.validation";
 
 const router = Router();
 
-router.post("/", jwtAuth, bookingController.createBooking);
+router.post("/",validateReq(bookingValidation) ,jwtAuth, bookingController.createBooking);
 
 router.get("/", jwtAuth, isAdmin, bookingController.getAllBookings);
 
--- src/app/modules/booking/booking.validation.ts
+++ src/app/modules/booking/booking.validation.ts
@@ -6,9 +6,9 @@ export const bookingValidation = z.object({
   date: z.string(),
   startTime: z.string(),
   endTime: z.string(),
-  user: z.string(),
-  facility: z.string(),
-  payableAmount: z.number(),
+  user: z.string().optional(),
+  facility: z.string().optional(),
+  payableAmount: z.number().optional(),
   isBooked: z
     .enum([
       BookingStatus.Unconfirmed,
